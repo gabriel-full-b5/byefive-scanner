417637d630e88917b59195e556d66cc07d01565d
gabriel-full-b5 <gabriel@byefive.com.br>
2024-09-13 15:25:06 -0300

    feat (app): initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar, StyleSheet } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.app}>
+      <Text>Byefive Scanner</Text>
+      <StatusBar />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
+    backgroundColor: '#FFF',
   },
-});
+})
